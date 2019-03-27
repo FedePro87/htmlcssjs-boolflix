9b5bfc077bdf6ca864938cbf043263e5ac41ffb2
FedePro87 <federicoprovenziani@gmail.com>
2019-03-27 14:00:58 +0100

    Addati button per le pagine. Addati titoli per le due categorie.

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
-function requestAjaxTmdb(action,type,myQuery) {
+var filmPage=1;
+var seriesPage=1;
+var isSearching=false;
+var searchQuery;
+
+function toggleSwitchers(data,type,page) {
+  var myRightSwitcher;
+  var myLeftSwitcher;
+
+  if (type=="tv") {
+    myRightSwitcher=$(".right-switcher.series-switcher");
+    myLeftSwitcher=$(".left-switcher.series-switcher");
+  } else {
+    myRightSwitcher=$(".right-switcher.film-switcher");
+    myLeftSwitcher=$(".left-switcher.film-switcher");
+  }
+
+  if (page>1) {
+    myLeftSwitcher.removeClass("hidden");
+  } else {
+    myLeftSwitcher.addClass("hidden");
+  }
+
+  if (data.page<data.total_pages) {
+    myRightSwitcher.removeClass("hidden");
+  } else {
+    myRightSwitcher.addClass("hidden");
+  }
+}
+
+function changeTitles(page,type,hasResults) {
+  var filmTitle=$("#film-title");
+  var seriesTitle=$("#series-title");
+
+  if (hasResults) {
+    if (type=="tv") {
+      if (page>1) {
+        seriesTitle.text("Serie TV - Più Popolari - Pagina " + page);
+      } else {
+        seriesTitle.text("Serie TV - Più Popolari");
+      }
+    } else {
+      if (page>1) {
+        filmTitle.text("Film - Più Popolari - Pagina " + page);
+      } else {
+        filmTitle.text("Film - Più Popolari");
+      }
+    }
+  } else {
+    if (type=="tv") {
+      seriesTitle.text("Serie TV");
+    } else {
+      filmTitle.text("Film");
+    }
+  }
+}
+
+function requestAjaxTmdb(action,type,myQuery,page) {
   var filmWrapper=$(".film-wrapper");
   var seriesWrapper=$(".series-wrapper");
   var noElementsMessage=document.createElement("h1");
@@ -8,17 +65,21 @@ function requestAjaxTmdb(action,type,myQuery) {
   $.ajax({
     url:"https://api.themoviedb.org/3/" + action + "/" + type,
     method:"GET",
-    data:{api_key:"e99307154c6dfb0b4750f6603256716d",query:myQuery,language:"it-IT"},
+    data:{api_key:"e99307154c6dfb0b4750f6603256716d",query:myQuery,language:"it-IT",page:page},
     success:function(data,state){
       if (data.results.length>0) {
         var results=data.results;
         populateUI(results,type);
+        toggleSwitchers(data,type,page);
+        changeTitles(page,type,true);
       } else {
         if (type=="tv") {
-          filmWrapper.append(noElementsMessage);
-        } else {
           seriesWrapper.append(noElementsMessage);
+        } else {
+          filmWrapper.append(noElementsMessage);
         }
+        toggleSwitchers(data,type,page);
+        changeTitles(page,type,false);
       }
     },
     error:function(request,state,error){
@@ -44,20 +105,17 @@ function populateUI(results,type) {
     }
 
     var stars=getStars(element.vote_average);
-    var noStars=5-stars;
-    var arrStars=getArrStars(stars);
-    var arrNoStars=getArrStars(noStars);
     var inData={};
 
-    if (element.title==null) {
+    if (type=="tv") {
       inData={
         itemId:"tv/" + element.id,
         type:"Serie TV",
         title:element.name,
         originalTitle:element.original_name,
         flag:getLanguageFlag(element.original_language),
-        stars:arrStars,
-        noStars:arrNoStars,
+        stars:getArrStars(stars),
+        noStars:getArrStars(5-stars),
         posterPathUrl:elementPathUrl,
         itemOverview:element.overview
       }
@@ -68,8 +126,8 @@ function populateUI(results,type) {
         title:element.title,
         originalTitle:element.original_title,
         flag:getLanguageFlag(element.original_language),
-        stars:arrStars,
-        noStars:arrNoStars,
+        stars:getArrStars(stars),
+        noStars:getArrStars(5-stars),
         posterPathUrl:elementPathUrl,
         itemOverview:element.overview
       }
@@ -129,43 +187,43 @@ function getLanguageFlag(myLanguage){
   return languageFlagUrl;
 }
 
-function initEnterSearch() {
-  var searchInput=$("#search-input");
-  var filmWrapper=$(".film-wrapper");
-  var seriesWrapper=$(".series-wrapper");
-
-  searchInput.on('keydown', function(e) {
-    var myQuery=searchInput.val();
-    if (e.which == 13) {
-      if (myQuery.length>0) {
-        filmWrapper.html("");
-        seriesWrapper.html("");
-        requestAjaxTmdb("search","movie",myQuery);
-        requestAjaxTmdb("search","tv",myQuery);
-      } else {
-        alert("Immettere un parametro di ricerca!");
-      }
-    }
-  });
-}
-
-function initMagnifierSearch() {
+function initSearch() {
   var searchInput=$("#search-input");
   var magnifier=$("#magnifier");
   var filmWrapper=$(".film-wrapper");
   var seriesWrapper=$(".series-wrapper");
+  var myQuery;
 
-  magnifier.click(function functionName() {
-    var myQuery=searchInput.val();
-    if (myQuery.length>0) {
-      filmWrapper.html("");
-      seriesWrapper.html("");
-      requestAjaxTmdb("search","movie",myQuery);
-      requestAjaxTmdb("search","tv",myQuery);
-    } else {
-      alert("Immettere un parametro di ricerca!");
+  searchInput.on('keydown',function(e) {
+    if (e.which == 13) {
+      myQuery=searchInput.val();
+      searchApi(myQuery);
+      searchInput.val("");
     }
   });
+
+  magnifier.click(function() {
+    myQuery=searchInput.val();
+    searchApi(myQuery);
+    searchInput.val("");
+  });
+}
+
+function searchApi(myQuery) {
+  var filmWrapper=$(".film-wrapper");
+  var seriesWrapper=$(".series-wrapper");
+  filmPage=1;
+  seriesPage=1;
+  searchQuery=myQuery;
+  if (myQuery.length>0) {
+    filmWrapper.html("");
+    seriesWrapper.html("");
+    requestAjaxTmdb("search","movie",myQuery,filmPage);
+    requestAjaxTmdb("search","tv",myQuery,seriesPage);
+    isSearching=true;
+  } else {
+    alert("Immettere un parametro di ricerca!");
+  }
 }
 
 function initOpenFilmPage() {
@@ -178,14 +236,76 @@ function initOpenFilmPage() {
 }
 
 function initStartUI() {
-  requestAjaxTmdb("discover","movie","");
-  requestAjaxTmdb("discover","tv","");
+  requestAjaxTmdb("discover","movie","",filmPage);
+  requestAjaxTmdb("discover","tv","",seriesPage);
+}
+
+function switchPage(next,type) {
+  var myWrapper;
+  var myTitle;
+  var myQuery;
+
+  if (isSearching) {
+    myAction="search";
+    myQuery=searchQuery;
+  } else {
+    myAction="discover";
+    myQuery="";
+  }
+
+  if (type=="tv") {
+    myWrapper=$(".series-wrapper");
+    myTitle=$("#series-title");
+    if (next) {
+      seriesPage++;
+    } else {
+      seriesPage--;
+    }
+    requestAjaxTmdb(myAction,"tv",myQuery,seriesPage);
+  } else {
+    myWrapper=$(".film-wrapper");
+    myTitle=$("#film-title");
+    if (next) {
+      filmPage++;
+    } else {
+      filmPage--;
+    }
+    requestAjaxTmdb(myAction,"movie",myQuery,filmPage);
+  }
+
+  $('html,body').animate({scrollTop: myTitle.offset().top},1000);
+  myWrapper.html("");
+}
+
+function initSwitchPages() {
+  var filmSwitcherLeft=$(".left-switcher.film-switcher");
+  var filmSwitcherRight=$(".right-switcher.film-switcher");
+  var seriesSwitcherLeft=$(".left-switcher.series-switcher");
+  var seriesSwitcherRight=$(".right-switcher.series-switcher");
+
+  filmSwitcherLeft.click(function() {switchPage(false,"movie");});
+  filmSwitcherRight.click(function() {switchPage(true,"movie");});
+  seriesSwitcherLeft.click(function() {switchPage(false,"tv");});
+  seriesSwitcherRight.click(function() {switchPage(true,"tv");});
+}
+
+function initBoolflixClick() {
+  var boolflixImg=$(".header-wrapper>img");
+
+  boolflixImg.click(function(){
+    var filmWrapper=$(".film-wrapper");
+    var seriesWrapper=$(".series-wrapper");
+    filmWrapper.html("");
+    seriesWrapper.html("");
+    initStartUI();
+  });
 }
 
 function init(){
   initStartUI();
-  initEnterSearch();
-  initMagnifierSearch();
+  initBoolflixClick();
+  initSwitchPages();
+  initSearch();
   initOpenFilmPage();
 }
 
